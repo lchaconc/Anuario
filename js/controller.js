d64a16aad879bb55abdd61dd249c6f140b8352b4
lchaconc <luis.chacon.campos@gmail.com>
2018-06-19 21:34:01 -0600

    incio de vistas
    - inicio
    -compas

--- js/controller.js
+++ js/controller.js
@@ -4,10 +4,43 @@ const m = new Model (),  v = new View ();
 
 $(document).ready(function () {
     console.log("ready");  
-    //loadMainScreen();
+    loadApp();
 });
 
-function loadMainScreen () {
+function loadApp () {
     v.startScreen($("#mainVisor"));
+    //Manejador de eventos menù superior
+    $(".nav-link").click(function (e) { 
+        e.preventDefault();
+        switch (this.id) {
+            case "pillsInicio":
+            v.startScreen($("#mainVisor"));
+            break;
+            case "pillsCompas":
+            v.mainStudents($("#mainVisor"));
+            break;
+            case "pillsProfes":
+            console.log("profes");
+            
+            break;
+            case "pillsCole":
+            console.log("Cole");
+            
+            break;
+            case "pillsGrupo":
+            console.log("Grupo");
+            
+            break;
+            
+        
+            default:
+            console.log("Selecciòn fuera de rango");
+            
+            break;
+        }
+
+        
+    });
     
 }
+
--- js/view.js
+++ js/view.js
@@ -10,3 +10,33 @@ View.prototype.startScreen = function (visor) {
     $(htmlContent).append("<br><h2>Generación 2018</h2>");
     $(visor).html(htmlContent);
   }
+
+  View.prototype.mainStudents = function (visor) { 
+      $(visor).empty();
+      var htmlContent = (
+        "<div class='col-sm-4'>" +
+            "<div class='alert alert-warning text-center' role='alert'>" +
+                "<H4>ESTUDIANTES</H4>" + 
+            "</div>" +
+        "</div>" +
+        "<div class='col-sm-8'>" +
+            "<div class='row'>" +
+                "<div class='col-sm-12' id='divEstudiante'>" +
+                    "<h3>Nombre del estudiante</h3>" +
+                "</div>" +
+            "</div>" +
+        "<div class='row'>" +
+            "<div class='col-sm-12' id='divBtnSecundaria'>" +
+                    "<button type='button' class='btn btn-outline-success btn-lg'>Así soy</button>" +
+                    "<button type='button' class='btn btn-outline-danger btn-lg '>Fotos</button>" +
+                    "<button type='button' class='btn btn-outline-warning btn-lg '>Testamento</button>" +
+            "</div>" +
+        "</div>" +
+        "<div class='row'>" + 
+            "<div class='col-sm-12' id='visorSecundario'>" +
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, aut architecto vero nemo corrupti natus at repellat similique ullam quibusdam expedita autem modi magni. Ipsa libero obcaecati ab officia? Libero." +
+            "</div>" + 
+        "</div>" +
+        "</div>");
+        $(visor).html(htmlContent);
+   }
